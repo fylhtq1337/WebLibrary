65a71ed472d5f4c28d35678b5952296c9f17938e
jktu8998 <cherkasin_oleg1337@rambler.ru>
2025-02-08 00:38:17 +0300

    init

---
+++ WebLibrary4/Models/Entities/Books.cs
@@ -0,0 +1,6 @@
+namespace WebLibrary4.Models.Entities;
+
+public class Books
+{
+    
+}
---
+++ WebLibrary4/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebLibrary4.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
